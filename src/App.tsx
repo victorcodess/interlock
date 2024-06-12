76eaeb68fc84606ee8c17cda95bbf63847c8500b
Victor Williams <victor.williams@stu.cu.edu.ng>
2024-06-12 14:50:16 +0100

    feat: implement navbar ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./navbar";
+import heroBg from "./assets/hero-bg.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <Navbar />
+      <section
+        // style={{ background: heroBg }}
+        className="min-h-[100vh] w-full relative flex items-center justify-center"
+      >
+        <img src={heroBg} className="absolute w-full h-full object-cover -z-10"/>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/navbar.tsx
@@ -0,0 +1,31 @@
+import logo from "./assets/logo.svg";
+
+const Navbar = () => {
+  return (
+    <nav className="fixed z-50 top-[19px] left-0 right-0 mx-[95px] h-[80px] flex items-center justify-between rounded-[32px] px-8 border-[4px] border-white/[0.38] bg-white/10 backdrop-blur-[4px] shadow-[0_57px_96px_-8px_rgba(87,92,95,0.25)]">
+      <div className="flex items-center justify-center gap-10">
+        <img src={logo} alt="" className="w-[142px] cursor-pointer" />
+        <div className="text-[#445568] text-[18px] font-semibold leading-[24px] flex gap-[32px] items-center justify-center">
+          <p className="cursor-pointer hover:text-[#445568]/50 transition-all">
+            Home
+          </p>
+          <p className="cursor-pointer hover:text-[#445568]/50 transition-all">
+            Products
+          </p>
+          <p className="cursor-pointer hover:text-[#445568]/50 transition-all">
+            Resources
+          </p>
+          <p className="cursor-pointer hover:text-[#445568]/50 transition-all">
+            Services
+          </p>
+        </div>
+      </div>
+
+      <button className="flex w-[154px] h-[48px] bg-[#131316] rounded-full outline outline-4 -outline-offset-[0px] outline-white/[55%] bor der-[8px] bo rder-white/[0.55] items-center justify-center text-white btn-shadow text-[18px] font-medium leading-[24px]">
+        Get App
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;

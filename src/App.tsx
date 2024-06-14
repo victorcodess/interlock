7e67d3a37cda5713a4415ca4560ac605b13a3fac
Victor Williams <victor.williams@stu.cu.edu.ng>
2024-06-14 18:26:32 +0100

    feat: build social proof section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Navbar from "./navbar";
 import Hero from "./components/hero";
 import Footer from "./footer";
+import SocialProof from "./components/social-proof";
 
 function App() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <SocialProof/>
       <Footer/>
     </main>
   );
--- src/components/social-proof.tsx
+++ src/components/social-proof.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import stripe from "../assets/stripe.svg";
+import upwork from "../assets/upwork.svg";
+import square from "../assets/square.svg";
+import amazon from "../assets/amazon.svg";
+import google from "../assets/google.svg";
+import linear from "../assets/linear.svg";
 
-const SocialProff = () => {
+const SocialProof = () => {
   return (
-    <div>SocialProff</div>
-  )
-}
+    <section className="w-full flex flex-col py-16 items-center justify-center gap-12">
+      <p className="text-[18px] text-[#2f2b43]/60 leading-[26px] tracking-[-0.216px]">
+        Trusted by the best
+      </p>
+      <div className="flex gap-6 items-center justify-center">
+        <img src={stripe} alt="" className="" />
+        <img src={upwork} alt="" className="" />
+        <img src={square} alt="" className="" />
+        <img src={amazon} alt="" className="" />
+        <img src={google} alt="" className="" />
+        <img src={linear} alt="" className="" />
+      </div>
+    </section>
+  );
+};
 
-export default SocialProff
+export default SocialProof;

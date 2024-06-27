6a9d5f3c41ab55909cc068ea4d8d795b4960cb81
Victor Williams <victor.williams@stu.cu.edu.ng>
2024-06-27 03:39:34 +0100

    fix: update lenis options

--- src/components/smooth-scrolling.tsx
+++ src/components/smooth-scrolling.tsx
@@ -2,7 +2,7 @@ import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScrolling({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, syncTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5 }}>
       {children}
     </ReactLenis>
   );

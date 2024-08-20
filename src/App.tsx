0c88678040d659f916e8abda1c564b5a659c409d
Victor Williams <victor.williams@stu.cu.edu.ng>
2024-08-20 21:59:59 +0100

    chore: try video upload

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import FirstCTA from "./components/first-cta";
 import WhyUs from "./components/why-us";
 import SecondCTA from "./components/second-cta";
 import SmoothScrolling from "./components/smooth-scrolling";
+import video from "./assets/ope-btn.mp4";
 
 function App() {
   return (
@@ -19,6 +20,15 @@ function App() {
         <FirstCTA />
         <WhyUs />
         <SecondCTA />
+        <video
+          className="h-[400px] max-w-none rounded-2xl md:h-[600px] xl:h-[772px]"
+          autoPlay
+          loop
+          playsInline
+          muted
+        >
+          <source src={video} type="video/mp4" />
+        </video>
         <Footer />
       </main>
     </SmoothScrolling>
